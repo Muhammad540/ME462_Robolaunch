8d0739bd182595a6172ceed9a4382b1469b020ac
Muhammad540 <thisisdeahmed@gmail.com>
2024-03-10 16:24:19 +0300

    removed the build, log and install files

--- led_ws/build/led_interfaces/led_interfaces__py/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ahmedmuhammad/led_ws/build/led_interfaces/led_interfaces__py
-# Build directory: /home/ahmedmuhammad/led_ws/build/led_interfaces/led_interfaces__py
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- led_ws/build/led_show/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/ahmedmuhammad/led_ws/install/led_show'
